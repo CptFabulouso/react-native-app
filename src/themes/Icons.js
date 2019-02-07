4d6bd2eb8200af2786977def11a0a96eeba96707
Pavel <pavelgric@gmail.com>
2019-02-07 13:08:22 +0100

    add Icons

--- src/themes/Icons.js
+++ src/themes/Icons.js
@@ -1,39 +1,34 @@
-// // @flow
-// import React from 'react';
+// @flow
+import React from 'react';
 
-// import Ionicons from 'react-native-vector-icons/Ionicons';
-// import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-// import type { Style } from 'flow-types';
+import type { Style } from 'flow-types';
 
-// type IconProps = {
-// 	size?: number,
-// 	color?: string,
-// 	style?: Style,
-// };
+type IconProps = {
+  size?: number,
+  color?: string,
+  style?: Style,
+};
 
-// const basicProps: IconProps = {
-// 	size: 30,
-// 	color: 'black',
-// };
+const basicProps: IconProps = {
+  size: 30,
+  color: 'black',
+};
 
-// const createIcon = (
-// 	Comp,
-// 	name,
-// 	props: IconProps,
-// 	defaultProps: IconProps = basicProps
-// ) => {
-// 	return <Comp name={name} {...defaultProps} {...props} />;
-// };
+const createIcon = (
+  Comp,
+  name,
+  props: IconProps,
+  defaultProps: IconProps = basicProps,
+) => {
+  return <Comp name={name} {...defaultProps} {...props} />;
+};
 
-// const icons = {
-// 	Person: (props: IconProps) => createIcon(Ionicons, 'ios-person', props),
-// 	Play: (props: IconProps) => createIcon(Ionicons, 'ios-play', props),
-// 	Pause: (props: IconProps) => createIcon(Ionicons, 'ios-pause', props),
-// 	Stop: (props: IconProps) => createIcon(MaterialIcons, 'stop', props),
-// 	Close: (props: IconProps) => createIcon(Ionicons, 'ios-close', props),
-// 	Info: (props: IconProps) =>
-// 		createIcon(Ionicons, 'ios-information-circle-outline', props),
-// };
+const icons = {
+  Main: (props: IconProps) => createIcon(Ionicons, 'ios-thumbs-up', props),
+  Settings: (props: IconProps) => createIcon(Ionicons, 'ios-settings', props),
+};
 
-// export default icons;
+export default icons;
